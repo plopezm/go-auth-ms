d2b7cd9d26fc6e2eba2faaa492b5c40988a2e2d1
Pablo LM <pabloplm@gmail.com>
2017-11-18 13:41:45 +0100

    Added comments to key creator cert

--- create_keys.sh
+++ create_keys.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
+#Creating JWT keys
 openssl genrsa -out jwtpriv.pem 4096
 openssl rsa -in jwtpriv.pem -pubout -out jwtpub.pem
 
+#Creating HTTPS key and cert
 openssl genrsa -out server.key 4096
 openssl req -new -x509 -sha512 -key server.key -out server.crt -days 3650
 
